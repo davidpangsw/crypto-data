f48c9c792f4d0191c85088a7026c6fc9076286cf
davidpangsw <davidpangsw@hotmail.com>
2025-03-16 16:24:00 -0700

    fix start script

--- influx/start.sh
+++ influx/start.sh
@@ -1,3 +1,8 @@
-sudo influxd &  
+# sudo influxd &  
+sudo systemctl restart influxdb
 
+# influx ping
+# Common commands for health-check
+# sudo systemctl status influxdb
+# journalctl -u influxdb
 # influx ping
